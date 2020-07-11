5da6566e0533ed9af20bd83e779a2fc1c53274b5
ZHANG ZHENGYI <zhangzhengyi1vivi@gmail.com>
2020-07-11 11:58:43 +0900

    album

---
+++ src/index.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.swiper-container', {
+  speed: 300,
+  direction:"horizontal",
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  on:{
+    init: function(){
+      swiperAnimateCache(this); //隐藏动画元素 
+      swiperAnimate(this); //初始化完成开始动画
+    }, 
+    slideChangeTransitionEnd: function(){ 
+      swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+      //this.slides.eq(this.activeIndex).find('.ani').removeClass('ani'); 动画只展现一次，去除ani类名
+    } 
+  }
+});    
